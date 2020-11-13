770def3ccf12032aebc5bfe48452548c71a008b1
fernando <fernandinhoo15@hotmail.com>
2020-11-12 23:10:40 -0300

    Versão final atualizada

--- esp32tempsensor/esp32tempsensor.ino
+++ esp32tempsensor/esp32tempsensor.ino
@@ -1,24 +1,12 @@
-#include <WiFi.h>
-#include <WebServer.h>
 #include "DHT.h"
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// Uncomment one of the lines below for whatever DHT sensor type you're using!
-#define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-
-/*Put your SSID & Password*/
-const char* ssid = "xxx";  // Enter SSID here
-const char* password = "xxx";  //Enter Password here
-
-WebServer server(80);
-
-// DHT Sensor
+#define DHTTYPE DHT11   // Tipo do Sensor DHT 11
+// DHT Sensor (Pino 4)
 uint8_t DHTPin = 4; 
                
-// Initialize DHT sensor.
+// Inicialização do sensor DHT.
 DHT dht(DHTPin, DHTTYPE);                
 
 float Temperature;
@@ -30,8 +18,9 @@ int i=0;
 int count=0;
 
 const int pinButton = 2; //pino do botão1
-const int pinButton2 = 15; //pino do botão1
+const int pinButton2 = 15; //pino do botão2
 
+//Timer
 hw_timer_t * timer0 = NULL;
 hw_timer_t * timer1 = NULL;
 portMUX_TYPE timerMux0 = portMUX_INITIALIZER_UNLOCKED;
@@ -41,9 +30,9 @@ void IRAM_ATTR onTimer0(){
   // Critical Code here
   portENTER_CRITICAL_ISR(&timerMux0);
   
-  Serial.println("TEMPORIZADOR LEITURA TEMP:");
+  Serial.println("30s - TEMPORIZADOR LEITURA TEMPERATURA: ON");
   i = 1;
-  Serial.println(i);
+//  Serial.println(i);
   
   portEXIT_CRITICAL_ISR(&timerMux0);
 }
@@ -52,18 +41,19 @@ void IRAM_ATTR onTimer1(){
   // Critical Code here
   portENTER_CRITICAL_ISR(&timerMux1);
 
-  Serial.println("TEMPORIZADOR BACKLIGHT OFF:");
+  Serial.println("10s - TEMPORIZADOR BACKLIGHT: OFF");
   count = 1;
-  Serial.println(count);
+//  Serial.println(count);
   
   portEXIT_CRITICAL_ISR(&timerMux1);
 }
 
+//Botao 1
 void botao1(){
   lcd.backlight();
   lcd.clear();// clear previous values from screen
   lcd.setCursor(0,0);
-  lcd.print("BOTAO LER AGORA!");
+  lcd.print("BOTAO1 LER AGORA!");
   temp3 = dht.readTemperature();
   temp2 = 0.00;
   temp1 = 0.00;
@@ -77,9 +67,12 @@ void botao1(){
   lcd.print("  ");
   lcd.setCursor(12,1);
   lcd.print(temp1);
-  delay(3000);
+  timerWrite(timer0, 0); //reseta o temporizador0  
+  timerWrite(timer1, 0); //reseta o temporizador1  
+//  delay(3000);
 }
 
+//Botao2
 void botao2() {
   lcd.backlight();
   lcd.clear();
@@ -104,72 +97,51 @@ void botao2() {
  
 void setup() {
   Serial.begin(115200);
+  //Define os botoes como entrada
   pinMode(pinButton, INPUT_PULLUP);
   pinMode(pinButton2, INPUT_PULLUP);
 
+//Inicializa os timers
   Serial.println("start timer 1");
   timer1 = timerBegin(1, 80, true);  // timer 1, MWDT clock period = 12.5 ns * TIMGn_Tx_WDT_CLK_PRESCALE -> 12.5 ns * 80 -> 1000 ns = 1 us, countUp
   timerAttachInterrupt(timer1, &onTimer1, true); // edge (not level) triggered 
-  timerAlarmWrite(timer1, 10500000, true); // 250000 * 1 us = 250 ms, autoreload true
+  timerAlarmWrite(timer1, 10000100, true); // 250000 * 1 us = 250 ms, autoreload true
 
   Serial.println("start timer 0");
   timer0 = timerBegin(0, 80, true);  // timer 0, MWDT clock period = 12.5 ns * TIMGn_Tx_WDT_CLK_PRESCALE -> 12.5 ns * 80 -> 1000 ns = 1 us, countUp
   timerAttachInterrupt(timer0, &onTimer0, true); // edge (not level) triggered 
   timerAlarmWrite(timer0, 30000000, true); // 2000000 * 1 us = 2 s, autoreload true
 
-//  // at least enable the timer alarms
   timerAlarmEnable(timer0); // enable
   timerAlarmEnable(timer1); // enable
 
-  //cod TIMERFIM
   delay(100);
-  
-  pinMode(DHTPin, INPUT);
 
+//Define o Sensor DHT como entrada  
+  pinMode(DHTPin, INPUT);
   dht.begin();              
 
-  Serial.println("Conectando em ");
-  Serial.println(ssid);
-
-  //connect to your local wi-fi network
-  WiFi.begin(ssid, password);
-
-  //check wi-fi is connected to wi-fi network
-  while (WiFi.status() != WL_CONNECTED) {
-  delay(1000);
-  Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi conectado...!");
-  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
-
-  server.on("/", handle_OnConnect);
-  server.onNotFound(handle_NotFound);
-
-  server.begin();
-  Serial.println("HTTP Server iniciado");
-
+//Inicializa o LCD
   lcd.begin();  
   lcd.backlight();
-  lcd.print("Fernando Torres");
+  lcd.print("Projeto ESP32");
   lcd.setCursor(0,1);
-  lcd.print("Sensor Temp. C");  
-  delay(2000); 
+  lcd.print("Sensor Temp. C");   
 }
 
 void loop() {
-  server.handleClient();
+//  server.handleClient();
 
-      if (count == 1) {
+      if (count == 1) { //quando o timer10s é ativado, executa esta função
         lcd.noBacklight();
-        Serial.println("Loop: noBacklight");
+        lcd.clear();
         count = 0;
-        delay(1000);
     }
 
-    if (i == 1) {
-        lcd.backlight(); //Acende o backlight
-        lcd.clear();// clear previous values from screen
+    if (i == 1) { //quando o timer30s é ativado, executa esta função
+        timerWrite(timer1, 0); //reseta o temporizador1  
+        lcd.backlight(); 
+        lcd.clear();
         lcd.print("Temp. Atual");
         lcd.setCursor(12,0);
         lcd.print(dht.readTemperature());
@@ -184,27 +156,19 @@ void loop() {
         lcd.print("  ");
         lcd.setCursor(12,1);
         lcd.print(temp1);
-        Serial.println("Loop: Temp");
         i = 0;
-        timerWrite(timer1, 0); //reseta o temporizador1  
-        delay(1000);
     }
   
-  //cODIGO TIMER
-  //vTaskDelay(portMAX_DELAY); // wait as much as posible ...
-
   //Botao1
       while(digitalRead(pinButton))
     {
         Serial.println("botão 1 pressionado = LER AGORA!");
         botao1();
-        //delay(100);
     }
           while(digitalRead(pinButton2))
     {
         Serial.println("botão 2 pressionado = BOTÃO RESET!");
         botao2();
-        //delay(100);
     }
 }
 
@@ -212,74 +176,4 @@ void printTemp() {
 temp1 = temp2;
 temp2 = temp3;
 temp3 = dht.readTemperature();
-}
-
-void handle_OnConnect() {
-
- Temperature = dht.readTemperature(); // Gets the values of the temperature
-  Humidity = dht.readHumidity(); // Gets the values of the humidity 
-  server.send(200, "text/html", SendHTML(Temperature,Humidity)); 
-}
-
-void handle_NotFound(){
-  server.send(404, "text/plain", "Not found");
-}
-
-String SendHTML(float TempCstat,float Humiditystat){
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<link href=\"https://fonts.googleapis.com/css?family=Open+Sans:300,400,600\" rel=\"stylesheet\">\n";
-  ptr +="<title>ESP32 Status da Temperatura </title>\n";
-  ptr +="<style>html { font-family: 'Open Sans', sans-serif; display: block; margin: 0px auto; text-align: center;color: #333333;}\n";
-  ptr +="body{margin-top: 50px;}\n";
-  ptr +="h1 {margin: 50px auto 30px;}\n";
-  ptr +=".side-by-side{display: inline-block;vertical-align: middle;position: relative;}\n";
-  ptr +=".humidity-icon{background-color: #3498db;width: 30px;height: 30px;border-radius: 50%;line-height: 36px;}\n";
-  ptr +=".humidity-text{font-weight: 600;padding-left: 15px;font-size: 19px;width: 160px;text-align: left;}\n";
-  ptr +=".humidity{font-weight: 300;font-size: 60px;color: #3498db;}\n";
-  ptr +=".temperature-icon{background-color: #f39c12;width: 30px;height: 30px;border-radius: 50%;line-height: 40px;}\n";
-  ptr +=".temperature-text{font-weight: 600;padding-left: 15px;font-size: 19px;width: 160px;text-align: left;}\n";
-  ptr +=".temperature{font-weight: 300;font-size: 60px;color: #f39c12;}\n";
-  ptr +=".superscript{font-size: 17px;font-weight: 600;position: absolute;right: -20px;top: 15px;}\n";
-  ptr +=".data{padding: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  
-   ptr +="<div id=\"webpage\">\n";
-   
-   ptr +="<h1>ESP32 Status da Temperatura </h1>\n";
-   ptr +="<div class=\"data\">\n";
-   ptr +="<div class=\"side-by-side temperature-icon\">\n";
-   ptr +="<svg version=\"1.1\" id=\"Layer_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n";
-   ptr +="width=\"9.915px\" height=\"22px\" viewBox=\"0 0 9.915 22\" enable-background=\"new 0 0 9.915 22\" xml:space=\"preserve\">\n";
-   ptr +="<path fill=\"#FFFFFF\" d=\"M3.498,0.53c0.377-0.331,0.877-0.501,1.374-0.527C5.697-0.04,6.522,0.421,6.924,1.142\n";
-   ptr +="c0.237,0.399,0.315,0.871,0.311,1.33C7.229,5.856,7.245,9.24,7.227,12.625c1.019,0.539,1.855,1.424,2.301,2.491\n";
-   ptr +="c0.491,1.163,0.518,2.514,0.062,3.693c-0.414,1.102-1.24,2.038-2.276,2.594c-1.056,0.583-2.331,0.743-3.501,0.463\n";
-   ptr +="c-1.417-0.323-2.659-1.314-3.3-2.617C0.014,18.26-0.115,17.104,0.1,16.022c0.296-1.443,1.274-2.717,2.58-3.394\n";
-   ptr +="c0.013-3.44,0-6.881,0.007-10.322C2.674,1.634,2.974,0.955,3.498,0.53z\"/>\n";
-   ptr +="</svg>\n";
-   ptr +="</div>\n";
-   ptr +="<div class=\"side-by-side temperature-text\">Temperatura</div>\n";
-   ptr +="<div class=\"side-by-side temperature\">";
-   ptr +=(int)TempCstat;
-   ptr +="<span class=\"superscript\">°C</span></div>\n";
-   ptr +="</div>\n";
-   ptr +="<div class=\"data\">\n";
-   ptr +="<div class=\"side-by-side humidity-icon\">\n";
-   ptr +="<svg version=\"1.1\" id=\"Layer_2\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n\"; width=\"12px\" height=\"17.955px\" viewBox=\"0 0 13 17.955\" enable-background=\"new 0 0 13 17.955\" xml:space=\"preserve\">\n";
-   ptr +="<path fill=\"#FFFFFF\" d=\"M1.819,6.217C3.139,4.064,6.5,0,6.5,0s3.363,4.064,4.681,6.217c1.793,2.926,2.133,5.05,1.571,7.057\n";
-   ptr +="c-0.438,1.574-2.264,4.681-6.252,4.681c-3.988,0-5.813-3.107-6.252-4.681C-0.313,11.267,0.026,9.143,1.819,6.217\"></path>\n";
-   ptr +="</svg>\n";
-   ptr +="</div>\n";
-   ptr +="<div class=\"side-by-side humidity-text\">Umidade</div>\n";
-   ptr +="<div class=\"side-by-side humidity\">";
-   ptr +=(int)Humiditystat;
-   ptr +="<span class=\"superscript\">%</span></div>\n";
-   ptr +="</div>\n";
-
-  ptr +="</div>\n";
-  ptr +="</body>\n";
-  ptr +="</html>\n";
-  return ptr;
 }
